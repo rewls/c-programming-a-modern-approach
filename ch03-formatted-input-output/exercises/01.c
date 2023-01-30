1d3d0b3800722242d6a9b8aabb83c5f7e5cd322d
rewls <rew0316@gmail.com>
2023-01-30 20:04:51 +0900

    Add ch3 formatted input/output

---
+++ ch03-formatted-input-output/exercises/01.c
@@ -0,0 +1,16 @@
+/*
+ * 01.c (Chapter 3, page 49, Exercise 1)
+ * Print given format string
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("|%6d,%4d|\n", 86, 1040);
+    printf("|%12.5e|\n", 30.253);
+    printf("|%.4f|\n", 83.162);
+    printf("|%-6.2g|\n", .0000009979);
+
+    return 0;
+}
