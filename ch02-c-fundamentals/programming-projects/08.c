31adc85146b30c94a3297e10b90c4adf6f71e1e0
rewls <rew0316@gmail.com>
2023-02-01 18:27:05 +0900

    Simplify code
    Remove macros that don't help readability.
    Simplify if conditions by unifying units.

--- ch02-c-fundamentals/programming-projects/08.c
+++ ch02-c-fundamentals/programming-projects/08.c
@@ -5,29 +5,27 @@
 
 #include <stdio.h>
 
-#define N_MONTH 12
-
 int main(void)
 {
-	float loan, interest_rate, monthly_interest_rate, monthly_payment;
+    float loan, interest_rate, monthly_interest_rate, monthly_payment;
 
-	printf("Enter amount of loan: ");
-	scanf("%f", &loan);
-	printf("Enter interest rate: ");
-	scanf("%f", &interest_rate);
-	printf("Enter monthly payment: ");
-	scanf("%f", &monthly_payment);
+    printf("Enter amount of loan: ");
+    scanf("%f", &loan);
+    printf("Enter interest rate: ");
+    scanf("%f", &interest_rate);
+    printf("Enter monthly payment: ");
+    scanf("%f", &monthly_payment);
 
-	monthly_interest_rate = interest_rate / N_MONTH;
-	loan *= 1 + (monthly_interest_rate / 100);
-	loan -= monthly_payment;
-	printf("Balance remaining after first payment: $%.2f\n", loan);
-	loan *= 1 + (monthly_interest_rate / 100);
-	loan -= monthly_payment;
-	printf("Balance remaining after second payment: $%.2f\n", loan);
-	loan *= 1 + (monthly_interest_rate / 100);
-	loan -= monthly_payment;
-	printf("Balance remaining after third payment: $%.2f\n", loan);
+    monthly_interest_rate = interest_rate / 12;
+    loan *= 1 + (monthly_interest_rate / 100);
+    loan -= monthly_payment;
+    printf("Balance remaining after first payment: $%.2f\n", loan);
+    loan *= 1 + (monthly_interest_rate / 100);
+    loan -= monthly_payment;
+    printf("Balance remaining after second payment: $%.2f\n", loan);
+    loan *= 1 + (monthly_interest_rate / 100);
+    loan -= monthly_payment;
+    printf("Balance remaining after third payment: $%.2f\n", loan);
 
-	return 0;
+    return 0;
 }
