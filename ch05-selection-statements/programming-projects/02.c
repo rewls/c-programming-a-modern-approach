b26f9f54ce2fc79e99b1781e1d7730f0b97597d5
rewls <rew0316@gmail.com>
2023-02-02 23:39:13 +0900

    Modify variable names

--- ch05-selection-statements/programming-projects/02.c
+++ ch05-selection-statements/programming-projects/02.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-    int hours, minutes;
+    int hour, minute;
 
     printf("Enter a 24-hour time: ");
-    scanf("%d:%d", &hours, &minutes);
+    scanf("%d:%d", &hour, &minute);
 
-    printf("Equivalent 12-hour time: %d:%.2d ", hours % 12 == 0 ? 12 : hours % 12, minutes);
+    printf("Equivalent 12-hour time: %d:%.2d ", hour % 12 == 0 ? 12 : hour % 12, minute);
 
-    if (hours >= 12) {
+    if (hour >= 12) {
 	printf("PM");
     } else {
 	printf("AM");
