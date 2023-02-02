30678c6370ea0236278110c50a50d0e76b677804
rewls <rew0316@gmail.com>
2023-02-02 23:40:15 +0900

    Fix if condition

--- ch05-selection-statements/programming-projects/08.c
+++ ch05-selection-statements/programming-projects/08.c
@@ -1,9 +1,8 @@
 /*
  * 08.c (Chapter 5, page 96, programming project 8)
- * Ask user to enter a time, then displays the departure and arrival times for the flight whose departure time is closest to that entered by the user.
+ * Ask user to enter a time, then displays the departure and arrival times for the flight
+ * whose departure time is closest to that entered by the user.
  */
-
-#include <stdio.h>
 
 int main(void)
 {
@@ -14,19 +13,20 @@ int main(void)
 
     time = hour * 60 + minute;
 
-    if (time < (temp1 = 8 * 60) + (temp1 + (temp2 = 9 * 60 + 43) / 2)) {
+    temp1 = 8 * 60;
+    if (time < temp1 + ((temp2 = 9 * 60 + 43) - temp1) / 2) {
 	printf("Closest departure time is 8:00 a.m., arriving at 10:16 a.m.");
-    } else if (time < temp2 + (temp2 + (temp1 = 11 * 60 + 19) / 2)) {
+    } else if (time < temp2 + ((temp1 = 11 * 60 + 19) - temp2) / 2) {
 	printf("Closest departure time is 9:43 a.m., arriving at 11:52 a.m.");
-    } else if (time < temp1 + (temp1 + (temp2 = 12 * 60 + 47) / 2)) {
+    } else if (time < temp1 + ((temp2 = 12 * 60 + 47) - temp1) / 2) {
 	printf("Closest departure time is 11:19 a.m., arriving at 1:31 p.m.");
-    } else if (time < temp2 + (temp2 + (temp1 = 14 * 60) / 2)) {
+    } else if (time < temp2 + ((temp1 = 14 * 60) - temp2) / 2) {
 	printf("Closest departure time is 12:47 p.m., arriving at 3:00 p.m.");
-    } else if (time < temp1 + (temp1 + (temp2 = 15 * 60 + 45) / 2)) {
+    } else if (time < temp1 + ((temp2 = 15 * 60 + 45) - temp1) / 2) {
 	printf("Closest departure time is 2:00 p.m., arriving at 4:08 p.m.");
-    } else if (time < temp2 + (temp2 + (temp1 = 19 * 60) / 2)) {
+    } else if (time < temp2 + ((temp1 = 19 * 60) - temp2) / 2) {
 	printf("Closest departure time is 3:45 p.m., arriving at 5:55 p.m.");
-    } else if (time < temp1 + (temp1 + (temp2 = 21 * 60 + 45) / 2)) {
+    } else if (time < temp1 + ((temp2 = 21 * 60 + 45) - temp1) / 2) {
 	printf("Closest departure time is 7:00 p.m., arriving at 9:20 p.m.");
     } else {
 	printf("Closest departure time is 9:45 p.m., arriving at 11:58 p.m.");
