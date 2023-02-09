06b808ee30d7f15f1d3d05966a9534dcbb7f2d76
rewls <rew0316@gmail.com>
2023-02-09 23:46:33 +0900

    Modify 09.c to record block

--- ch08-arrays/programmng-projects/09.c
+++ ch08-arrays/programmng-projects/09.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>  /* srand(), rand() */
 #include <time.h>  /* time() */
+#include <stdbool.h>
 
 #define SIZE 10
 #define N_DIRECTIONS 4
@@ -13,10 +14,11 @@
 int main(void)
 {
     char walk[SIZE][SIZE];
-    int current_row, current_column, next_row, next_culumn, direction, block_count,
+    int current_row, current_column, next_row, next_culumn, direction,
 	i, j;
     const int offset_row[N_DIRECTIONS] = {-1, 0, 1, 0},
 	  offset_column[N_DIRECTIONS] = {0, 1, 0, -1};
+    bool block[N_DIRECTIONS] = {false};
     char ch;
 
     for (i = 0; i < SIZE; i++) {
@@ -29,7 +31,6 @@ int main(void)
     current_row = 0;
     current_column = 0;
     ch = 'A';
-    block_count = 0;
     while (ch <= 'Z') {
 	walk[current_row][current_column] = ch;
 	direction = rand() % 4;
@@ -41,17 +42,13 @@ int main(void)
 	    current_row = next_row;
 	    current_column = next_culumn;
 	    ch++;
-	    block_count = 0;
-	} else {
-	    block_count++;
-	}
-	if (block_count >= N_DIRECTIONS) {
 	    for (i = 0; i < N_DIRECTIONS; i++) {
-		next_row = current_row + offset_row[i];
-		next_culumn = current_column + offset_column[i];
-		if (next_row < SIZE && next_row >= 0
-			&& next_culumn < SIZE && next_culumn >= 0
-			&& walk[next_row][next_culumn] == '.') {
+		block[i] = false;
+	    }
+	} else {
+	    block[direction] = true;
+	    for (i = 0; i < N_DIRECTIONS; i++) {
+		if (block[i] == false) {
 		    break;
 		}
 	    }
