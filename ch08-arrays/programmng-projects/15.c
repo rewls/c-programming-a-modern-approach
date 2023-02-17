50250bb9894b63695efc9169c41cfdf37f577471
rewls <rew0316@gmail.com>
2023-02-17 17:13:44 +0900

    Modify to use isupper, islower

--- ch08-arrays/programmng-projects/15.c
+++ ch08-arrays/programmng-projects/15.c
@@ -4,6 +4,7 @@
  */
 
 #include <stdio.h>
+#include <ctype.h>
 
 #define SIZE 80
 #define N_ALPHABETS 26
@@ -11,7 +12,7 @@
 int main(void)
 {
     char message[SIZE], ch;
-    int shift_amount, i, count;
+    int shift, i, count;
 
     printf("Enter message to be encrypted: ");
     for (i = 0; (ch = getchar()) != '\n'; i++) {
@@ -20,14 +21,14 @@ int main(void)
     count = i;
 
     printf("Enter shift amount (1-25): ");
-    scanf("%d", &shift_amount);
+    scanf("%d", &shift);
 
     printf("Encrypted message: ");
     for (i = 0; i < count; i++) {
-        if (message[i] >= 'A' && message[i] <= 'Z') {
-            putchar(((message[i] - 'A') + shift_amount) % N_ALPHABETS + 'A');
-        } else if (message[i] >= 'a' && message[i] <= 'z') {
-            putchar(((message[i] - 'a') + shift_amount) % N_ALPHABETS + 'a');
+        if (isupper(message[i])) {
+            putchar(((message[i] - 'A') + shift) % N_ALPHABETS + 'A');
+        } else if (islower(message[i])) {
+            putchar(((message[i] - 'a') + shift) % N_ALPHABETS + 'a');
         } else {
             putchar(message[i]);
         }
